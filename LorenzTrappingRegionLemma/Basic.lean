2cb8f9b0cc1d680a55ef5138bcac7e154ed306e9
Endi Hajdari <113209883+endi-hajdari@users.noreply.github.com>
2026-07-19 17:31:31 -0400

    Update Basic.lean

--- LorenzTrappingRegionLemma/Basic.lean
+++ LorenzTrappingRegionLemma/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 ICARM Summer School. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: ICARM Summer School participants
+Authors: Abdullah Ahmed, Endi Hajdari, Pankaj Singh, and Swati
 -/
 
 import Mathlib.Analysis.Calculus.ContDiff.Defs
